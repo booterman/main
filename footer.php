ac6ad695466597e4ee6dfd4a39c6137e1bb88afd
Arseniy <4375585@gmail.com>
2015-07-10 21:48:46 +0300

    start

---
+++ footer.php
@@ -0,0 +1,7 @@
+	<footer>
+		<p>© <a href="http://evrochistka-plus.ru/">evrochistka-plus.ru</a>, 2015<br/>
+		Все права защищены
+		</p>
+	</footer>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE  html>
+<head>
+	<title></title>
+	<meta charset="utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="stylesheet" href="style/style.css" type="text/css" charset="utf-8" />
+	<title>ÅÂÐÎ×ÈÑÒÊÀ + ÊËÞ×ÑÅÐÂÈÑ</title>
+</head>
+<body>
+	<header>
+		<div id="l_header">
+		&nbsp;
+		</div>
+		<div id="header">
+				<img id="logo" src="/images/topindex.png" alt="ÅÂÐÎ×ÈÑÒÊÀ + ÊËÞ×ÑÅÐÂÈÑ" />
+		</div>
+		<div id="r_header">
+		&nbsp;
+		</div>
+	</header>
+	<span id="c_header">&nbsp;</span>
